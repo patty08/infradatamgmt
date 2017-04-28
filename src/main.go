53fea52918f06fcd3f48c8ca2ae24c3db450804f
Patty RANDRIAMBOLOLONA <patty.randria8@gmail.com>
2017-04-28 15:30:23 +0200

    Add rooter base implementation

---
+++ src/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+   "os"
+   "time"
+)
+
+// Data structure of informations channels
+// action stand for action event, service stand for services whish in output, info all informations
+type infoIN struct{
+   Action   string
+   Services []string
+   Data     map[string] string
+}
+
+//interface for agent setting
+type AgentIn interface {
+   addEventListener(c chan *infoIN, who string)
+}
+
+//interface for service setting
+type ServiceOut interface {
+   setAction(action string, data map[string]string)
+}
+
+// Exit listening and close stream
+func closeListener()  {
+   println("Exit")
+   os.Exit(0)
+}
+
+func process(i *infoIN) {
+   if i != nil {
+	panic("data not found")
+   }
+
+   // TODO !!!
+   // serviceOut setAction(i.Action, i.Data)
+}
+
+// start agent and open channels in and out stream
+// input channel an listen to the structure value stream
+// initialise des channels d'ecoute puis recupère les données du channel, traite les données inspecter puis envoi la configuration en sortie
+func main() {
+   // open input channel and listening
+   listener := make(chan *infoIN)
+   // START ALL AGENTS
+   // agentDocker.startEventListener(listener)
+   for {
+	  response := <-listener
+	  go process(response)
+	  time.Sleep (time.Second * 1)
+   }
+
+   closeListener()
+}

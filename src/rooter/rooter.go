7dd30e14811496d40dee92359ea0772b868b754a
Xawirses <xawirses@gmail.com>
2017-05-04 14:18:14 +0200

    Add unit test of rooter

--- src/rooter/rooter.go
+++ src/rooter/rooter.go
@@ -1,25 +1,23 @@
 package rooter
 
 import (
-   "os"
-   "time"
-   "errors"
-   "agent"
-   "client"
-   "io/ioutil"
-   "log"
-   "fmt"
+	"agent"
+	"client"
+
+	"errors"
+	"time"
 )
 
 // *** Strategy Agent ***
 
 // interface for agent setting
 type AgentIn interface {
-   AddEventListener(c chan *agent.InfoIN, who string) error
+	AddEventListener(c chan *agent.InfoIN, who string) error
 }
+
 // structure for calling agent
 type sAgentIn struct {
-   AgentIn AgentIn
+	AgentIn AgentIn
 }
 
 // *** End Strategy Agent ***
@@ -27,68 +25,56 @@ type sAgentIn struct {
 
 //interface for client out settings
 type ClientOut interface {
-   SetAction(info *agent.InfoIN) error
+	SetAction(info *agent.InfoIN) error
 }
 
 // Structure for calling client
 type sClientOut struct {
-   aClientOut ClientOut
+	aClientOut ClientOut
 }
 
 // *** End Strategy client ***
 
-// function close listening
-func closeListener()  {
-   println("Exit")
-   os.Exit(0)
+func process(i *agent.InfoIN) error {
+	if i == nil {
+		return errors.New("InfoIn Structure Error on process")
+	}
+	//namefile := "service/config.txt"
+	//parseConfig(namefile)
+	var err error
+	switch i.Data["client"] {
+	case "docker":
+		agent := sClientOut{client.ClientDocker{}}
+		err = agent.aClientOut.SetAction(i)
+	default:
+		agent := sClientOut{client.ClientDocker{}}
+		err = agent.aClientOut.SetAction(i)
+	}
+
+	return err
 }
 
-func process(i *agent.InfoIN) {
-   //fmt.Println("recupéré: ", i.Action,i.Services, i.Data)
-   if i == nil {
-	  errors.New("errors....")
-   }
-   //namefile := "service/config.txt"
-   //parseConfig(namefile)
-
-   fmt.Println(i.Data)
-   switch i.Data["client"]{
-   case "docker":
-	  {
-		///agent := sServiceDocker{client.ClientDocker{}}
-		//go agent.aClientDocker.SetAction(i.Action,i.Services, i.Data)
-		 agent := sClientOut{client.ClientDocker{}}
-		 go agent.aClientOut.SetAction(i)
-	  }
-   default:
-	  fmt.Println("aaa", i.Data)
-	  agent := sClientOut{client.ClientDocker{}}
-	  go agent.aClientOut.SetAction(i)
-   }
-}
-
-func parseConfig(namefile string) {
-   stream, err := ioutil.ReadFile(namefile)
-   if err != nil {
-	  log.Fatal(err)
-   }
-   lireFichier := string(stream)
-   fmt.Println(lireFichier)
-}
+// TODO implementation config file for rooter
+/*func parseConfig(namefile string) {
+	stream, err := ioutil.ReadFile(namefile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lireFichier := string(stream)
+	fmt.Println(lireFichier)
+}*/
 
 // start agent and open channels in and out stream
 // input channel an listen to the structure value stream
 // initialise des channels d'ecoute puis recupère les données du channel, traite les données inspecter puis envoi la configuration en sortie
 func Start() {
-   // open input channel and listening
-   listener := make(chan *agent.InfoIN)
-   // START ALL AGENTS
-   agentDocker := agent.AgentDocker{}
-   a := sAgentIn{agentDocker}
-   go a.AgentIn.AddEventListener(listener, "unix:///var/run/docker.sock")
-   for {
-	  go process(<- listener)
-	  time.Sleep (time.Second * 1)
-   }
-   closeListener()
+	// open input channel and listening
+	listener := make(chan *agent.InfoIN)
+	// START ALL AGENTS
+	a := sAgentIn{agent.AgentDocker{}}
+	go a.AgentIn.AddEventListener(listener, "unix:///var/run/docker.sock")
+	for {
+		go process(<-listener)
+		time.Sleep(time.Second * 1)
+	}
 }

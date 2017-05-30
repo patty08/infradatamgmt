8ead3c40f00e9072fcd1aef82e1b5f9cca9d423a
bigby347 <alexandreputzu@gmail.com>
2017-05-30 10:02:23 +0200

    Add metric unit test

--- service/metrics_test.go
+++ service/metrics_test.go
@@ -3,8 +3,9 @@ package service
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"github.com/elgs/gostrgen"
+	"os"
 )
-
 
 func TestSetActionMetrics(t *testing.T) {
 	service := ServiceMetrics{}
@@ -12,5 +13,34 @@ func TestSetActionMetrics(t *testing.T) {
 	err := service.GetAction("create", map[string]string {})
 	assert.Nil(t, err)
 
-	//TODO write unit test
+}
+func TestFormatMetricHostName(t *testing.T)  {
+
+	ip := "127.0.0.1"
+	out := formatMetricHostName(ip)
+	assert.Equal(t,"hosts: [\"127.0.0.1\"]",out,"Error Reformat Host name")
+	ip = ""
+	out =formatMetricHostName(ip)
+	assert.Equal(t,"hosts: [\"\"]",out,"Error Empty host")
+	ip,err := gostrgen.RandGen(10, gostrgen.Lower,"","")
+	assert.Nil(t,err)
+	out =formatMetricHostName(ip)
+	assert.Equal(t,"hosts: [\""+ ip +"\"]",out,"Error Reformat Host name")
+
+}
+
+func TestDetachMetricsConfiguration(t *testing.T)  {
+
+	data := map[string]string{}
+
+	data["application_type"]= "test"
+	data["id"]= "file"
+
+	_, err := os.Create(data["application_type"]+"_"+data["id"]+".yml")
+	assert.NotNil(t,err)
+
+	detachMetricConfiguration(data)
+	_,err = os.Open(data["application_type"]+"_"+data["id"]+".yml")
+	assert.NotNil(t,err)
+
 }
